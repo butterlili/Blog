4cdfb8a6cb18418e2c4ad25abd75c51ced727883
butterlili <saint.souveraine@gmail.com>
2025-06-12 09:21:29 +0700

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
-import store from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      {/* The Provider component makes the Redux store available to any nested components that need to access the Redux state. */}
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+
+);
+
+{/* The Provider component makes the Redux store available to any nested components that need to access the Redux state. */}
       {/* The store prop is the Redux store that was created in the store.js file. */}
       {/* This allows the App component and its children to connect to the Redux store and access the state or dispatch actions. */}
-    <App />
-    </Provider>
-  </StrictMode>,
-)
+    
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content area.</p>
+    </div>
+   
   )
 }
